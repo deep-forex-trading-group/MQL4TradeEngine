7f55f0fd639f5a717bc93b168e3be1cb3b36b856
jackieQizhu <jackiecococa@gmail.com>
2021-07-27 23:39:23 +0800

    Renames config directory to Upper Case

--- pub_config.sh
+++ pub_config.sh
@@ -2,6 +2,6 @@ mkdir -p ../tester/files/config
 mkdir -p ../Files/config
 mkdir -p ./Files/config
 
-cp config/* ../tester/files/config/.
-cp config/* ../Files/config/.
-cp config/* ./Files/config/.
+cp Config/* ../tester/files/config/.
+cp Config/* ../Files/config/.
+cp Config/* ./Files/config/.
